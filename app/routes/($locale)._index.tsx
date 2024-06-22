67772fe9ea18565615b826c4a6e4b86871f60b3b
hackiku <ivan@pipewriter.io>
2024-06-22 17:52:25 +0200

    choco meta title

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -8,7 +8,7 @@ import type {
 } from 'storefrontapi.generated';
 
 export const meta: MetaFunction = () => {
-  return [{title: 'Hydrogen | Home'}];
+  return [{title: 'Sweetchoice 🍫 Initial Commit'}];
 };
 
 export async function loader(args: LoaderFunctionArgs) {
