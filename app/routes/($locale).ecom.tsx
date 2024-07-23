65e9241a507ab733b4c5c913815d589f1e2684a1
hackiku <ivan@pipewriter.io>
2024-07-23 03:29:57 +0200

    flowbite runs in /ecom

--- app/routes/($locale).ecom.tsx
+++ app/routes/($locale).ecom.tsx
@@ -1,23 +1,31 @@
 // app/routes/($locale).ecom.tsx
 
-import { Button, Footer } from "flowbite-react";
+import { Button, Footer, Tooltip } from "flowbite-react";
 
 
 export default function Ecom() {
 	return (
-		<h2 className="text-5xl">Ecom Flowbite</h2>
-		<Button>Click me</Button>;
+		<div>
+			<h2 className="text-5xl pt-12">Ecom Flowbite</h2>
+			<Tooltip content="Flowbite is awesome">
+				<Button color={"light"}>
+					Hover to find out
+				</Button>
+			</Tooltip>
+			
+		<section className="fixed left-0 bottom-0 w-screen bg-gray-100">
+			<Footer container>
+				<Footer.Copyright href="#" by="Flowbite™" year={2022} />
+				<Footer.LinkGroup>
+					<Footer.Link href="#">About</Footer.Link>
+					<Footer.Link href="#">Privacy Policy</Footer.Link>
+					<Footer.Link href="#">Licensing</Footer.Link>
+					<Footer.Link href="#">Contact</Footer.Link>
+				</Footer.LinkGroup>
+			</Footer>
+		</section>
 
-		// <Footer container>
-		// 	<Footer.Copyright href="#" by="Flowbite™" year={2022} />
-		// 	<Footer.LinkGroup>
-		// 		<Footer.Link href="#">About</Footer.Link>
-		// 		<Footer.Link href="#">Privacy Policy</Footer.Link>
-		// 		<Footer.Link href="#">Licensing</Footer.Link>
-		// 		<Footer.Link href="#">Contact</Footer.Link>
-		// 	</Footer.LinkGroup>
-		// </Footer>
-
+		</div>
 
 	);
 }
